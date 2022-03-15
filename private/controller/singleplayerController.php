49b67ee325457657a8bb86d6c93c2731502e5ece
DJJayT <jason.talge@icloud.com>
2022-03-15 10:31:39 +0100

    Footer wieder hinzugefügt

--- private/controller/singleplayerController.php
+++ private/controller/singleplayerController.php
@@ -9,7 +9,7 @@ class singleplayerController extends baseController
         $this->renderViewAction("general/generalhead.html");
         $this->renderViewAction("singleplayer/singleplayerhead.html");
         $this->renderViewAction("singleplayer/singleplayerbody.html");
-        //$this->renderViewAction("general/generalfooter.html");
+        $this->renderViewAction("general/generalfooter.html");
         //Hier Code schreiben
     }
 }
